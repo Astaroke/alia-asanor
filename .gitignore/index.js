8d07558469bb61cfebef9b2dcda92a1be524ed59
Astaroke <61854317+Astaroke@users.noreply.github.com>
2020-03-06 02:50:22 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,4 +1,4 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login("Njg1MjM3Njg0NjYyNTY3MDc1.XmGJhA.Bf-_cgi0I553ak6IoEwV2TObCBI");
+client.login("Njg1MjM3Njg0NjYyNTY3MDc1.XmGrpA.9PYm82mg4libAys-ei2U_akjAgY");
